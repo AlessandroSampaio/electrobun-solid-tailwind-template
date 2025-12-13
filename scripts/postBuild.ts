5f4cd38e6af1a1bfac827de9e953185bf1ca50d0
Alessandro Sampaio <ars@MacBook-Air-de-Alessandro.local>
2025-12-12 22:56:00 -0300

    refactor: replace require statements with import

--- scripts/postBuild.ts
+++ scripts/postBuild.ts
@@ -1,30 +1,36 @@
-console.log('post build script');
+console.log("post build script");
 
-import { execSync } from 'child_process';
-
-const path = require('path');
-const esbuild = require('esbuild');
-const { solidPlugin } = require('esbuild-plugin-solid');
+import { execSync } from "child_process";
+import esbuild from "esbuild";
+import { solidPlugin } from "esbuild-plugin-solid";
+import path from "path";
 
 const { ELECTROBUN_BUILD_DIR, ELECTROBUN_APP_NAME } = process.env;
 const platform = process.platform;
 
 const APP_BUNDLE_FOLDER =
-  platform === 'win32'
+  platform === "win32"
     ? path.join(ELECTROBUN_BUILD_DIR!, ELECTROBUN_APP_NAME!)
     : path.join(ELECTROBUN_BUILD_DIR!, `${ELECTROBUN_APP_NAME}.app`);
 
 // buildEnvironment
 const BUILD_FOLDER = path.resolve(
-  platform === 'win32' ?
-  path.join(APP_BUNDLE_FOLDER, 'resources', 'app', 'views', 'main') :
-  path.join(APP_BUNDLE_FOLDER, 'Contents', 'Resources', 'app', 'views', 'main'),
+  platform === "win32"
+    ? path.join(APP_BUNDLE_FOLDER, "resources", "app", "views", "main")
+    : path.join(
+        APP_BUNDLE_FOLDER,
+        "Contents",
+        "Resources",
+        "app",
+        "views",
+        "main",
+      ),
 );
 
 // Build main renderer
 await esbuild.build({
   entryPoints: [
-    path.resolve(path.join('./src', 'renderers', 'main', 'index.tsx')),
+    path.resolve(path.join("./src", "renderers", "main", "index.tsx")),
   ],
   // outDir: path.join(appPath, "build"),
   outfile: path.join(BUILD_FOLDER, `index.js`),
@@ -34,24 +40,24 @@ await esbuild.build({
   // jsxImportSource: "solid-js",
   // jsxFactory: "Solid.h", // use Solid's h function for JSX
   // jsxFragment: "Solid.Fragment", // use
-  jsxFactory: 'Solid.createElement',
-  jsxFragment: 'Solid.Fragment',
+  jsxFactory: "Solid.createElement",
+  jsxFragment: "Solid.Fragment",
   // TODO: do we need to detect or have different filenames for renderers with and without node integration?
   // or can we just always compile with node as the target and let user turn it on or off in new BrowserWindow()
   // NOTE: you still need to set this to browser even if nodeIntegration is true
-  platform: 'browser',
+  platform: "browser",
   //   target: "node14",
-  format: 'esm',
+  format: "esm",
   loader: {
-    '.tts': 'file',
-    '.ttf': 'file',
-    '.node': 'file',
+    ".tts": "file",
+    ".ttf": "file",
+    ".node": "file",
     // ".svg": "text",
   },
 });
-console.log('--------> esbuild main');
+console.log("--------> esbuild main");
 
-console.log('Compiling tailwind css for main renderer...');
+console.log("Compiling tailwind css for main renderer...");
 
 /*
 Usage:
@@ -65,9 +71,9 @@ Options:
    -h, --help               Display usage information
          */
 // path.resolve(path.join("./src", "renderers", "main", "index.tsx"));
-const cssInPath = path.join('./src/renderers', 'main', 'index.css');
+const cssInPath = path.join("./src/renderers", "main", "index.css");
 const cssOutPath = path.join(
-  BUILD_FOLDER.replace('(', '\\(').replace(')', '\\)'),
+  BUILD_FOLDER.replace("(", "\\(").replace(")", "\\)"),
   `tailwind.css`,
 );
 // const contentPath = path.join("./src/renderers", "main", "*.tsx");
